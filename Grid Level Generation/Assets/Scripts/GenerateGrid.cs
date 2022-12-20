329e2a2be8cdd1d679dbba278f253c1293f0f0b9
jake-d37 <jake.william.deandrade@gmail.com>
2022-12-20 14:09:00 +1100

    code done about to make prefabs then test

--- Grid Level Generation/Assets/Scripts/GenerateGrid.cs
+++ Grid Level Generation/Assets/Scripts/GenerateGrid.cs
@@ -11,12 +11,13 @@ public class GenerateGrid : MonoBehaviour
     [SerializeField]
     private int minWidth = 5;
     [SerializeField]
-    private GameObject[] gridObject;
+    private GameObject[] objPrefab;
     [SerializeField]
     private float elementSize = 1f;
 
     GridObject[,] gridElements;
     List<GridObject> gridElementsToCollapse;
+    List<GridObject> gridElementsCollapsed;
 
     void Start () {
         Generate(gridLength, gridWidth);
@@ -24,6 +25,12 @@ public class GenerateGrid : MonoBehaviour
 
 
     public void Generate(int l, int w) {
+        foreach (GridObject i in gridElementsCollapsed){
+            gridElementsCollapsed.Remove(i);
+        }
+        foreach (GridObject i in gridElementsToCollapse){
+            gridElementsToCollapse.Remove(i);
+        }
         //destroy all game objects in scene
 
         if (w < minWidth)
@@ -36,14 +43,14 @@ public class GenerateGrid : MonoBehaviour
         for (int i = 0; i < l; i++){
             for (int j = 0; j < w; j++){
                 gridElements[i,j] = GenerateElement(i,j,l,w);
-                Instantiate(gridElements[i,j].model, new Vector3(i * elementSize, 0f, j * elementSize), Quaternion.identity);
+                Instantiate(gridElements[i,j].model, new Vector3(i * elementSize, 0f, j * elementSize), gridElements[i,j].model.transform.rotation);
 
                 //add element just generated into the list of elements to collapse
+                gridElementsToCollapse.Add(gridElements[i,j]);
 
                 //collapse all surrounding elements of the matrix to water
                 if (i == 0 || j == 0 || j == w-1 || i == l-1) {
-                    gridElements [i,j].type = 0;
-                    //remove element from list of elements to collapse
+                    CollapseElement(gridElements[i,j], 0);
                 }
             }
         }
@@ -52,8 +59,42 @@ public class GenerateGrid : MonoBehaviour
     }
 
     void ObserveGrid() {
-        //read elements to collapse lists
-        //collapse all possibilities to suit neighbour matrices
+        //update possibilities of elements neighbouring collapsed elements
+        foreach (GridObject element in gridElementsCollapsed){
+            List<int> topPos = element.neighbourMatrix.m[1,2];
+            List<int> botPos = element.neighbourMatrix.m[3,2];
+            List<int> lefPos = element.neighbourMatrix.m[2,1];
+            List<int> rigPos = element.neighbourMatrix.m[2,3];
+
+            int x = Mathf.FloorToInt(element.number.x);
+            int y = Mathf.FloorToInt(element.number.y);
+            //top
+            foreach (int i in gridElements[x, y+1].possibilities){
+                if (!topPos.Contains(i)){
+                    gridElements[x, y+1].possibilities.Remove(i);
+                }
+            }
+            //bottom
+            foreach (int i in gridElements[x, y-1].possibilities){
+                if (!botPos.Contains(i)){
+                    gridElements[x, y-1].possibilities.Remove(i);
+                }
+            }
+            //left
+            foreach (int i in gridElements[x-1, y].possibilities){
+                if (!lefPos.Contains(i)){
+                    gridElements[x-1, y].possibilities.Remove(i);
+                }
+            }
+            //right
+            foreach (int i in gridElements[x+1, y].possibilities){
+                if (!rigPos.Contains(i)){
+                    gridElements[x+1, y].possibilities.Remove(i);
+                }
+            }
+        }
+
+        //find element (to collapse) that has lowest entropy
         GridObject smallestEntropy = new GridObject ();
         foreach (GridObject go in gridElementsToCollapse){
             if (smallestEntropy.possibilities.Count != 0){
@@ -65,18 +106,32 @@ public class GenerateGrid : MonoBehaviour
             }
         }
 
-        //collapse smallest entropy to a random type within its possibilities (if any, otherwise leave blank)
-        //remove smallest entropy from the grid elements to collapse list
+        //collapse smallest entropy to a random type within its possibilities
+        CollapseElement(smallestEntropy, Mathf.FloorToInt(Random.Range (0, smallestEntropy.possibilities.Count)));
 
         //repeat observation process until there are no elements to collapse
         if (gridElementsToCollapse.Count > 0){
             ObserveGrid ();
+        } else {
+            return;
         }
+    }
+
+    void CollapseElement (GridObject element, int T){
+        //collapse type
+        element.type = T;
+        element.model = objPrefab[T];
+        //update neighbour matrix now that type is collapsed
+        element.neighbourMatrix = element.FindMatrix(T);
+        //update lists
+        gridElementsToCollapse.Remove(element);
+        gridElementsCollapsed.Add(element);
     }
 
     GridObject GenerateElement(int i, int j, int l, int w) {
         GridObject element = new GridObject();
 
+        element.number = new Vector2 (i,j);
         element.possibilities = new List<int> {0,1,2,3,4,5,6,7,8,9,10,11,12,13};
 
         return element;
--- Grid Level Generation/Assets/Scripts/GridObject.cs
+++ Grid Level Generation/Assets/Scripts/GridObject.cs
@@ -8,17 +8,128 @@ public class GridObject
     public List<int> possibilities;
     public NeighbourMatrix neighbourMatrix = new NeighbourMatrix ();
     public GameObject model;
+    public Vector2 number = Vector2.zero;
 
     public NeighbourMatrix FindMatrix(int T){
         NeighbourMatrix nm = new NeighbourMatrix ();
 
-        if (T == 0){
+        /*if (T == 0){
             nm.m = new List<int>[3,3] {
                 {new List<int>{0,8,13}, new List<int>{0,8}, new List<int>{0,8,12}},
                 {new List<int>{0,6}, new List<int>{0}, new List<int>{0,4}},
                 {new List<int>{0,2,11}, new List<int>{0,2}, new List<int>{0,2,10}}
             }; 
+        }*/
+        
+        switch (T)
+        {
+            case 0:
+                nm.m = new List<int>[3,3] {
+                    {new List<int>{0}, new List<int>{0,8}, new List<int>{0}},
+                    {new List<int>{0,6}, new List<int>{0}, new List<int>{0,4}},
+                    {new List<int>{0}, new List<int>{0,2}, new List<int>{0}}
+                }; 
+                break;
+            case 1:
+                nm.m = new List<int>[3,3] {
+                    {new List<int>{0}, new List<int>{0}, new List<int>{0}},
+                    {new List<int>{0}, new List<int>{0}, new List<int>{2,3,10}},
+                    {new List<int>{0}, new List<int>{4,10,7}, new List<int>{0}}
+                }; 
+                break;
+            case 2:
+                nm.m = new List<int>[3,3] {
+                    {new List<int>{0}, new List<int>{0}, new List<int>{0}},
+                    {new List<int>{2,1,11}, new List<int>{0}, new List<int>{2,3,10}},
+                    {new List<int>{0}, new List<int>{5,12,13}, new List<int>{0}}
+                }; 
+                break;
+            case 3:
+                nm.m = new List<int>[3,3] {
+                    {new List<int>{0}, new List<int>{0}, new List<int>{0}},
+                    {new List<int>{1,2,11}, new List<int>{0}, new List<int>{0}},
+                    {new List<int>{0}, new List<int>{6,9,11}, new List<int>{0}}
+                }; 
+                break;
+            case 4:
+                nm.m = new List<int>[3,3] {
+                    {new List<int>{0}, new List<int>{1,4,12}, new List<int>{0}},
+                    {new List<int>{0}, new List<int>{0}, new List<int>{5,6,11,13}},
+                    {new List<int>{0}, new List<int>{4,7,10}, new List<int>{0}}
+                }; 
+                break;
+            case 5:
+                nm.m = new List<int>[3,3] {
+                    {new List<int>{0}, new List<int>{2,5,10,11}, new List<int>{0}},
+                    {new List<int>{4,5,10,12}, new List<int>{0}, new List<int>{5,6,11,13}},
+                    {new List<int>{0}, new List<int>{5,8,12,13}, new List<int>{0}}
+                }; 
+                break;
+            case 6:
+                nm.m = new List<int>[3,3] {
+                    {new List<int>{0}, new List<int>{3,6,13}, new List<int>{0}},
+                    {new List<int>{4,5,10,12}, new List<int>{0}, new List<int>{0}},
+                    {new List<int>{0}, new List<int>{6,9,11}, new List<int>{0}}
+                }; 
+                break;
+            case 7:
+                nm.m = new List<int>[3,3] {
+                    {new List<int>{0}, new List<int>{1,4,12}, new List<int>{0}},
+                    {new List<int>{0}, new List<int>{0}, new List<int>{8,9,12}},
+                    {new List<int>{0}, new List<int>{0}, new List<int>{0}}
+                }; 
+                break;
+            case 8:
+                nm.m = new List<int>[3,3] {
+                    {new List<int>{0}, new List<int>{2,5,10,11}, new List<int>{0}},
+                    {new List<int>{7,8,11}, new List<int>{0}, new List<int>{8,9,10}},
+                    {new List<int>{0}, new List<int>{0}, new List<int>{0}}
+                }; 
+                break;
+            case 9:
+                nm.m = new List<int>[3,3] {
+                    {new List<int>{0}, new List<int>{3,6,13}, new List<int>{0}},
+                    {new List<int>{7,8,13}, new List<int>{0}, new List<int>{0}},
+                    {new List<int>{0}, new List<int>{0}, new List<int>{0}}
+                }; 
+                break;
+            case 10:
+                nm.m = new List<int>[3,3] {
+                    {new List<int>{0}, new List<int>{1,4,12}, new List<int>{0}},
+                    {new List<int>{1,2,11}, new List<int>{0}, new List<int>{5,6,11,13}},
+                    {new List<int>{0}, new List<int>{5,8,12,13}, new List<int>{0}}
+                }; 
+                break;
+            case 11:
+                nm.m = new List<int>[3,3] {
+                    {new List<int>{0}, new List<int>{3,6,13}, new List<int>{0}},
+                    {new List<int>{4,5,10,12}, new List<int>{0}, new List<int>{2,3,10}},
+                    {new List<int>{0}, new List<int>{5,8,12,13}, new List<int>{0}}
+                }; 
+                break;
+            case 12:
+                nm.m = new List<int>[3,3] {
+                    {new List<int>{0}, new List<int>{2,5,10,11}, new List<int>{0}},
+                    {new List<int>{7,8,13}, new List<int>{0}, new List<int>{5,6,11,13}},
+                    {new List<int>{0}, new List<int>{4,7,10}, new List<int>{0}}
+                }; 
+                break;
+            case 13:
+                nm.m = new List<int>[3,3] {
+                    {new List<int>{0}, new List<int>{2,5,10,11}, new List<int>{0}},
+                    {new List<int>{4,5,10,12}, new List<int>{0}, new List<int>{8,9,12}},
+                    {new List<int>{0}, new List<int>{6,9,11}, new List<int>{0}}
+                }; 
+                break;
+            default:
+                nm.m = new List<int>[3,3] {
+                    {new List<int>{0}, new List<int>{0,8}, new List<int>{0}},
+                    {new List<int>{0,6}, new List<int>{0}, new List<int>{0,4}},
+                    {new List<int>{0}, new List<int>{0,2}, new List<int>{0}}
+                }; 
+                break;
         }
+
         //repeat for all 14 types (T == 0-13)
 
         return nm;
